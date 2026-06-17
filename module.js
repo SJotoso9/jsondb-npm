5c0d683b34b112ed59a83a7fd7d05616a61c7936
Josué Ronaldo <josueronaldo360@gmail.com>
2026-06-17 11:34:21 -0300

    v1.0.3-beta1

--- module.js
+++ module.js
@@ -1,5 +1,8 @@
 /*
-    USE new METHOD TO MAKE A NEW DATABASE
+    USE new KEYWORD TO MAKE A NEW DATABASE 
+
+    REMEMBER: THIS IS A DEVELOP BRANCH ONLY MAKE ANY CHANGES HERE AND WHEN DONE
+    IT WILL BE ON THE MAIN BRANCH (with a release too)
 */
 
 module.exports = require('./lib/jsondb')
